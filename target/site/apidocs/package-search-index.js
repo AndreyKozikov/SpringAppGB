4d8978520c4c5d75ab19f4fa61e3f1d18c34b147
Andrey Kozikov <nwisdom@mail.ru>
2024-10-27 10:34:30 +0500

    APIDOCs added

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.SpringAppGB"},{"l":"com.example.SpringAppGB.controllers"},{"l":"com.example.SpringAppGB.controllers.api"},{"l":"com.example.SpringAppGB.model"},{"l":"com.example.SpringAppGB.repository.interfaces"},{"l":"com.example.SpringAppGB.services"}];updateSearchResults();
